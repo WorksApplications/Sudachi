5837b6feeb1bc6623251ba2ed9c68f19a03fb365
kazuma-t <takaoka_k@worksap.co.jp>
2017-07-12 14:09:52 +0900

    untabify

--- src/test/java/jp/co/worksap/nlp/sudachi/MockMorphemeTest.java
+++ src/test/java/jp/co/worksap/nlp/sudachi/MockMorphemeTest.java
@@ -19,102 +19,102 @@ public class MockMorphemeTest {
 
     @Before
     public void setUp() {
-	dictionary = new MockDictionary(null);
-	Tokenizer tokenizer = dictionary.create();
-	aUnits = tokenizer.tokenize(Tokenizer.SplitMode.A, text);
-	bUnits = tokenizer.tokenize(Tokenizer.SplitMode.B, text);
-	cUnits = tokenizer.tokenize(Tokenizer.SplitMode.C, text);
+        dictionary = new MockDictionary(null);
+        Tokenizer tokenizer = dictionary.create();
+        aUnits = tokenizer.tokenize(Tokenizer.SplitMode.A, text);
+        bUnits = tokenizer.tokenize(Tokenizer.SplitMode.B, text);
+        cUnits = tokenizer.tokenize(Tokenizer.SplitMode.C, text);
     }
 
     @After
     public void tearDown() {
-	dictionary.close();
+        dictionary.close();
     }
 
     @Test
     public void begin() {
-	assertEquals(0, aUnits.get(0).begin());
-	assertEquals(6, aUnits.get(6).begin());
-	assertEquals(2, bUnits.get(1).begin());
-	assertEquals(4, bUnits.get(3).begin());
-	assertEquals(2, cUnits.get(1).begin());
-	assertEquals(5, cUnits.get(2).begin());
+        assertEquals(0, aUnits.get(0).begin());
+        assertEquals(6, aUnits.get(6).begin());
+        assertEquals(2, bUnits.get(1).begin());
+        assertEquals(4, bUnits.get(3).begin());
+        assertEquals(2, cUnits.get(1).begin());
+        assertEquals(5, cUnits.get(2).begin());
     }
 
     @Test
     public void end() {
-	assertEquals(1, aUnits.get(0).end());
-	assertEquals(7, aUnits.get(6).end());
-	assertEquals(3, bUnits.get(1).end());
-	assertEquals(5, bUnits.get(3).end());
-	assertEquals(5, cUnits.get(1).end());
-	assertEquals(7, cUnits.get(2).end());
+        assertEquals(1, aUnits.get(0).end());
+        assertEquals(7, aUnits.get(6).end());
+        assertEquals(3, bUnits.get(1).end());
+        assertEquals(5, bUnits.get(3).end());
+        assertEquals(5, cUnits.get(1).end());
+        assertEquals(7, cUnits.get(2).end());
     }
 
     @Test
     public void surface() {
-	assertEquals("今", aUnits.get(0).surface());
-	assertEquals("今日", bUnits.get(0).surface());
-	assertEquals("はいい", cUnits.get(1).surface());
+        assertEquals("今", aUnits.get(0).surface());
+        assertEquals("今日", bUnits.get(0).surface());
+        assertEquals("はいい", cUnits.get(1).surface());
     }
 
     @Test
     public void partOfSpeech() {
-	String[] pos1
-	    = { "名詞", "普通名詞" ,"一般", "*", "*", "*", };
-	String[] pos2
-	    = { "動詞", "一般", "*", "*", "五段-サ行", "連用形-一般", };
-	String[] pos3
-	    = { "名詞", "固有名詞", "地名", "一般", "*", "*", };
+        String[] pos1
+            = { "名詞", "普通名詞" ,"一般", "*", "*", "*", };
+        String[] pos2
+            = { "動詞", "一般", "*", "*", "五段-サ行", "連用形-一般", };
+        String[] pos3
+            = { "名詞", "固有名詞", "地名", "一般", "*", "*", };
 
-	assertArrayEquals(pos1, aUnits.get(0).partOfSpeech());
-	assertArrayEquals(pos3, aUnits.get(2).partOfSpeech());
-	assertArrayEquals(pos2, bUnits.get(1).partOfSpeech());
-	assertArrayEquals(pos3, cUnits.get(2).partOfSpeech());
+        assertArrayEquals(pos1, aUnits.get(0).partOfSpeech());
+        assertArrayEquals(pos3, aUnits.get(2).partOfSpeech());
+        assertArrayEquals(pos2, bUnits.get(1).partOfSpeech());
+        assertArrayEquals(pos3, cUnits.get(2).partOfSpeech());
     }
 
     @Test
     public void dictionaryForm() {
-	assertEquals("今", aUnits.get(0).dictionaryForm());
-	assertEquals("日る", aUnits.get(1).dictionaryForm());
-	assertEquals("はいいる", cUnits.get(1).dictionaryForm());
+        assertEquals("今", aUnits.get(0).dictionaryForm());
+        assertEquals("日る", aUnits.get(1).dictionaryForm());
+        assertEquals("はいいる", cUnits.get(1).dictionaryForm());
     }
 
     @Test
     public void normalizedForm() {
-	assertEquals("今", aUnits.get(0).normalizedForm());
-	assertEquals("今日", bUnits.get(0).normalizedForm());
-	assertEquals("はいい", cUnits.get(1).normalizedForm());
+        assertEquals("今", aUnits.get(0).normalizedForm());
+        assertEquals("今日", bUnits.get(0).normalizedForm());
+        assertEquals("はいい", cUnits.get(1).normalizedForm());
     }
 
     @Test
     public void reading() {
-	assertEquals("ア", aUnits.get(0).reading());
-	assertEquals("アア", bUnits.get(0).reading());
-	assertEquals("アアア", cUnits.get(1).reading());
+        assertEquals("ア", aUnits.get(0).reading());
+        assertEquals("アア", bUnits.get(0).reading());
+        assertEquals("アアア", cUnits.get(1).reading());
     }
 
     @Test
     public void split() {
-	Morpheme a = aUnits.get(0);
-	assertEquals(1, a.split(Tokenizer.SplitMode.A).size());
-	assertEquals(1, a.split(Tokenizer.SplitMode.B).size());
-	assertEquals(1, a.split(Tokenizer.SplitMode.C).size());
-	Morpheme b = bUnits.get(0);
-	assertEquals(2, b.split(Tokenizer.SplitMode.A).size());
-	assertEquals(1, b.split(Tokenizer.SplitMode.B).size());
-	assertEquals(1, b.split(Tokenizer.SplitMode.C).size());
-	Morpheme c = cUnits.get(1);
-	assertEquals(3, c.split(Tokenizer.SplitMode.A).size());
-	assertEquals(3, c.split(Tokenizer.SplitMode.B).size());
-	assertEquals(1, c.split(Tokenizer.SplitMode.C).size());
+        Morpheme a = aUnits.get(0);
+        assertEquals(1, a.split(Tokenizer.SplitMode.A).size());
+        assertEquals(1, a.split(Tokenizer.SplitMode.B).size());
+        assertEquals(1, a.split(Tokenizer.SplitMode.C).size());
+        Morpheme b = bUnits.get(0);
+        assertEquals(2, b.split(Tokenizer.SplitMode.A).size());
+        assertEquals(1, b.split(Tokenizer.SplitMode.B).size());
+        assertEquals(1, b.split(Tokenizer.SplitMode.C).size());
+        Morpheme c = cUnits.get(1);
+        assertEquals(3, c.split(Tokenizer.SplitMode.A).size());
+        assertEquals(3, c.split(Tokenizer.SplitMode.B).size());
+        assertEquals(1, c.split(Tokenizer.SplitMode.C).size());
     }
 
     @Test
     public void isOOV() {
-	assertFalse(aUnits.get(0).isOOV());
-	assertFalse(bUnits.get(0).isOOV());
-	assertFalse(cUnits.get(1).isOOV());
+        assertFalse(aUnits.get(0).isOOV());
+        assertFalse(bUnits.get(0).isOOV());
+        assertFalse(cUnits.get(1).isOOV());
     }
 }
 
--- src/test/java/jp/co/worksap/nlp/sudachi/MockTokenizerTest.java
+++ src/test/java/jp/co/worksap/nlp/sudachi/MockTokenizerTest.java
@@ -12,37 +12,37 @@ public class MockTokenizerTest {
 
     @Before
     public void setUp() {
-	dictionary = new MockDictionary(null);
+        dictionary = new MockDictionary(null);
     }
 
     @After
     public void tearDown() {
-	dictionary.close();
+        dictionary.close();
     }
 
     @Test
     public void tokenize() {
-	Tokenizer t = dictionary.create();
-	List<Morpheme> ms = t.tokenize("今日はいい天気");
-	assertNotNull(ms);
-	assertEquals(3, ms.size());
+        Tokenizer t = dictionary.create();
+        List<Morpheme> ms = t.tokenize("今日はいい天気");
+        assertNotNull(ms);
+        assertEquals(3, ms.size());
     }
 
     @Test
     public void tokenizeA() {
-	Tokenizer t = dictionary.create();
-	List<Morpheme> ms = t.tokenize(Tokenizer.SplitMode.A,
-				       "今日はいい天気");
-	assertNotNull(ms);
-	assertEquals(7, ms.size());
+        Tokenizer t = dictionary.create();
+        List<Morpheme> ms = t.tokenize(Tokenizer.SplitMode.A,
+                                       "今日はいい天気");
+        assertNotNull(ms);
+        assertEquals(7, ms.size());
     }
 
     @Test
     public void tokenizeB() {
-	Tokenizer t = dictionary.create();
-	List<Morpheme> ms = t.tokenize(Tokenizer.SplitMode.B,
-				       "今日はいい天気");
-	assertNotNull(ms);
-	assertEquals(5, ms.size());
+        Tokenizer t = dictionary.create();
+        List<Morpheme> ms = t.tokenize(Tokenizer.SplitMode.B,
+                                       "今日はいい天気");
+        assertNotNull(ms);
+        assertEquals(5, ms.size());
     }
 }
