addfa52ec867db77e15b65c8de6a4c2e9856aacd
takaoka_k <takaoka_k@worksap.co.jp>
2017-07-17 17:09:37 +0900

    Add a default implement.

--- src/main/java/com/worksap/nlp/sudachi/Tokenizer.java
+++ src/main/java/com/worksap/nlp/sudachi/Tokenizer.java
@@ -3,7 +3,10 @@ package com.worksap.nlp.sudachi;
 import java.util.List;
 public interface Tokenizer {
     public List<Morpheme> tokenize(SplitMode mode, String text);
-    public List<Morpheme> tokenize(String text);
+
+    public default List<Morpheme> tokenize(String text) {
+        return tokenize(SplitMode.C, text);
+    }
 
     public static enum SplitMode { A, B, C; }
 }
