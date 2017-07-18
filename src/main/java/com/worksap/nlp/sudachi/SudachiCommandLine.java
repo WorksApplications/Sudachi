178e0e3c8b2c10c3f6048ef985ed96ff0b6f0a18
takaoka_k <takaoka_k@worksap.co.jp>
2017-07-18 20:20:08 +0900

    Add a command line tool.

---
+++ src/main/java/com/worksap/nlp/sudachi/SudachiCommandLine.java
@@ -0,0 +1,43 @@
+package com.worksap.nlp.sudachi;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class SudachiCommandLine {
+
+    public static void main(String[] args) throws IOException {
+        Tokenizer.SplitMode mode = Tokenizer.SplitMode.C;
+        if (args.length > 0) {
+            switch (args[0]) {
+            case "A":
+                mode = Tokenizer.SplitMode.A;
+                break;
+            case "B":
+                mode = Tokenizer.SplitMode.B;
+                break;
+            default:
+                mode = Tokenizer.SplitMode.C;
+                break;
+            }
+        }
+
+        BufferedReader reader
+            = new BufferedReader(new InputStreamReader(System.in));
+
+        JapaneseDictionary dict = new JapaneseDictionary();
+        Tokenizer tokenizer = dict.create();
+
+        while (true) {
+            String line = reader.readLine();
+            if (line == null) {
+                break;
+            }
+            for (Morpheme m : tokenizer.tokenize(mode, line)) {
+                System.out.print(m.surface() + "\t");
+                System.out.print(String.join(",", m.partOfSpeech()) + "\t");
+                System.out.println(m.normalizedForm());
+            }
+        }
+    }
+}
