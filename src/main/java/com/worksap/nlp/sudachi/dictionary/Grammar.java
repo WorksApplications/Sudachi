4f7794690153ad742e824d98776ddcb8fae37fe8
kazuma-t <takaoka_k@worksap.co.jp>
2017-07-19 19:48:31 +0900

    Add a method for getting POS name from POS id

--- src/main/java/com/worksap/nlp/sudachi/dictionary/Grammar.java
+++ src/main/java/com/worksap/nlp/sudachi/dictionary/Grammar.java
@@ -3,6 +3,7 @@ package com.worksap.nlp.sudachi.dictionary;
 public interface Grammar {
     public int getPartOfSpeechSize();
     public String[] getPartOfSpeechString(short posId);
+    public short getPartOfSpeechId(String[] pos);
     public short getConnectCost(int leftId, int rightId);
     public short[] getBOSParameter();
     public short[] getEOSParameter();
