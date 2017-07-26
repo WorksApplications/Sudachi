81e609b055916a33b9c022f1d1681be773e0bd12
Yuichiro Shimizu <shimizu_yu@worksap.co.jp>
2017-07-26 22:14:31 +0900

    Change rewriting process (use external file) & some fix

--- src/main/java/com/worksap/nlp/sudachi/DefaultInputTextPlugin.java
+++ src/main/java/com/worksap/nlp/sudachi/DefaultInputTextPlugin.java
@@ -1,36 +1,26 @@
 
 package com.worksap.nlp.sudachi;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.nio.charset.StandardCharsets;
 import java.text.Normalizer;
 import java.text.Normalizer.Form;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DefaultInputTextPlugin implements InputTextPlugin {
     
-    private static final char[][] ignoreNormalizeBounds = {
-        {'Ⅰ', 'ⅿ'},
-        {'⺀', '⿕'},
-        {'豈', '龎'}
-    };
-    private static final char[][] joinedCharBounds = {
-        {'ﾞ', 'ｳ', 'ｳ'},
-        {'ﾞ', 'ｶ', 'ﾄ'},
-        {'ﾞ', 'ﾊ', 'ﾎ'},
-        {'ﾟ', 'ﾊ', 'ﾎ'},
-        {'ﾞ', 'う', 'う'},
-        {'ﾞ', 'か', 'と'},
-        {'ﾞ', 'は', 'ほ'},
-        {'ﾟ', 'は', 'ほ'},
-        {'゙', 'う', 'う'},
-        {'゙', 'か', 'と'},
-        {'゙', 'は', 'ほ'},
-        {'゚', 'は', 'ほ'}
-    };
+    public String rewriteDef;
+    
+    private List<String> ignoreNormalizeList = new ArrayList<>();
+    private List<String[]> replaceCharList = new ArrayList<>();
     
     @Override
     public void setUp() throws IOException {
-        // nothing to do in this plug-in
-        // or may be better if reads char[][] lists as an external file here
+        readRewriteBounds(rewriteDef);
     }
     
     @Override
@@ -40,31 +30,30 @@ public class DefaultInputTextPlugin implements InputTextPlugin {
         String substr;
         String originalText = text.getText();
         for (int i = 0; i < originalText.length(); i++) {
-            ch = originalText.charAt(i);
-            // check if surrogate pair
+            substr = originalText.substring(i);
+            // 1. replace char without normalize
+            for (int j = 0; j < replaceCharList.size(); j++) {
+                if (substr.startsWith(replaceCharList.get(j)[0])) {
+                    text.replace(i, i + replaceCharList.get(j)[0].length(), replaceCharList.get(j)[1]);
+                    break;
+                }
+            }
+            // 2. normalize
+            // 2-1. check if surrogate pair
+            ch = substr.charAt(0);
             if ((ch >= Character.MIN_HIGH_SURROGATE) && (ch <= Character.MAX_HIGH_SURROGATE)) {
                 charLength = 2;
             } else if ((ch >= Character.MIN_LOW_SURROGATE) && (ch <= Character.MAX_LOW_SURROGATE)) {
+                //    do nothing if lower surrogate
                 continue;
             } else {
                 charLength = 1;
             }
-            // 1. joinable char -> joined    e.g. kana & sound mark
-            if (i != originalText.length() - 1) {
-                for (int j = 0; j < joinedCharBounds.length; j++) {
-                    if (originalText.charAt(i + 1) == joinedCharBounds[j][0]) {
-                        if ((ch >= joinedCharBounds[j][1]) && (ch <= joinedCharBounds[j][2])) {
-                            charLength++;
-                            break;
-                        }
-                    }
-                }
-            }
-            // 2. capital alphabet (not only latin but greek, cyrillic, etc.) -> small
-            substr = originalText.substring(i, i + charLength).toLowerCase();
-            // 3. normalize (except in ignoreNormalizeBounds)
-            //    e.g. full-width alphabet -> half-width / ligature -> separated / etc.
-            if (isCharInIgnoreNormalizeBounds(ch)) {
+            // 2-2. capital alphabet (not only latin but greek, cyrillic, etc) -> small
+            substr = substr.substring(0, charLength).toLowerCase();
+            // 2-3. normalize (except in ignoreNormalize)
+            //    e.g. full-width alphabet -> half-width / ligature / etc.
+            if (ignoreNormalizeList.contains(substr)) {
                 text.replace(i, i + charLength, substr);
             }
             else {
@@ -73,12 +62,39 @@ public class DefaultInputTextPlugin implements InputTextPlugin {
         }
     }
     
-    private boolean isCharInIgnoreNormalizeBounds(char ch) {
-        for (int i = 0; i < ignoreNormalizeBounds.length; i++) {
-            if ((ch >= ignoreNormalizeBounds[i][0]) && (ch <= ignoreNormalizeBounds[i][1])) {
-                return true;
+    private void readRewriteBounds(String rewriteDef) throws IOException {
+        try (
+            FileInputStream fin = new FileInputStream(rewriteDef);
+            LineNumberReader reader
+                = new LineNumberReader(new InputStreamReader(fin, StandardCharsets.UTF_8))
+        ){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.matches("\\s*") || line.startsWith("#")) {
+                    continue;
+                }
+                String[] cols = line.split("\\s+");
+                // ignored normalize list
+                if (cols.length == 1) {
+                    ignoreNormalizeList.add(cols[0]);
+                }
+                // replace char list
+                else if (cols.length == 2) {
+                    for (String[] definedPair : replaceCharList) {
+                        if (cols[0].equals(definedPair[0])) {
+                            throw new RuntimeException(
+                                cols[0] + " is already defined at line " + reader.getLineNumber()
+                            );
+                        }
+                    }
+                    replaceCharList.add(new String[]{cols[0], cols[1]});
+                }
+                else {
+                    throw new RuntimeException(
+                        "invalid format at line " + reader.getLineNumber()
+                    );
+                }
             }
         }
-        return false;
     }
 }
