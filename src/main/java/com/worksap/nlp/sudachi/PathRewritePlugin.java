85ba3d2bc543200d99696541986228a90992880e
takaoka_k <takaoka_k@worksap.co.jp>
2017-07-27 21:23:38 +0900

    Allow PathRewritePlugin to read a lattice for searching nodes have a same range

--- src/main/java/com/worksap/nlp/sudachi/PathRewritePlugin.java
+++ src/main/java/com/worksap/nlp/sudachi/PathRewritePlugin.java
@@ -2,13 +2,7 @@ package com.worksap.nlp.sudachi;
 
 import java.util.List;
 
-public abstract class PathRewritePlugin {
+public interface PathRewritePlugin {
 
-    public abstract void rewrite(List<LatticeNode> path);
-
-    LatticeNode createNode() {
-        LatticeNode node = new LatticeNodeImpl();
-        return node;
-    }
-
+    public void rewrite(List<LatticeNode> path, Lattice lattice);
 }
