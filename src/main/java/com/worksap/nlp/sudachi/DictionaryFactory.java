49fa759eb9198fc0eb05028284d314a26d568231
takaoka_k <takaoka_k@worksap.co.jp>
2017-07-31 20:27:41 +0900

    Add javadocs

--- src/main/java/com/worksap/nlp/sudachi/DictionaryFactory.java
+++ src/main/java/com/worksap/nlp/sudachi/DictionaryFactory.java
@@ -2,8 +2,18 @@ package com.worksap.nlp.sudachi;
 
 import java.io.IOException;
 
+/**
+ * Build a {@link Dictionary} instance from a dictionary file.
+ */
 public class DictionaryFactory {
 
+    /**
+     * Creates <tt>Dictionary</tt> by read a dictionary file.
+     *
+     * @param settings settings in JSON string
+     * @return {@link Dictionary}
+     * @throws IOException if reading a file is failed
+     */
     public Dictionary create(String settings) throws IOException {
         return new JapaneseDictionary(settings);
     }
--- src/main/java/com/worksap/nlp/sudachi/Tokenizer.java
+++ src/main/java/com/worksap/nlp/sudachi/Tokenizer.java
@@ -3,14 +3,54 @@ package com.worksap.nlp.sudachi;
 import java.io.PrintStream;
 import java.util.List;
 
+/**
+ * A tokenizer of morphological analysis.
+ */
 public interface Tokenizer {
+
+    /**
+     * Tokenize a text.
+     *
+     * This method tokenizes a input text as a sentence.
+     * When the text is long, it required a lot of memory.
+     *
+     * @param mode a mode of splitting
+     * @param text input text
+     * @return  a result of tokenizing
+     */
     public List<Morpheme> tokenize(SplitMode mode, String text);
 
+    /**
+     * Tokenize a text.
+     *
+     * Tokenize a text with {@link SplitMode}.C.
+     *
+     * @param text input text
+     * @return  a result of tokenizing
+     * @see #tokenize(SplitMode,String)
+     */
     public default List<Morpheme> tokenize(String text) {
         return tokenize(SplitMode.C, text);
     }
 
+    /**
+     * Prints a lattice structure of analyzing.
+     *
+     * @param output an output of printing
+     */
     public void setDumpOutput(PrintStream output);
 
-    public static enum SplitMode { A, B, C; }
+    /**
+     * A mode of splitting
+     */
+    public static enum SplitMode {
+        /** short mode */
+        A,
+
+        /** middle mode */
+        B,
+
+        /** long mode */
+        C,
+    }
 }
