e55cfbbe6e182926416235f666975bb4acc66bed
kazuma-t <takaoka_k@worksap.co.jp>
2017-07-31 20:27:41 +0900

    Add javadocs

--- src/main/java/com/worksap/nlp/sudachi/Dictionary.java
+++ src/main/java/com/worksap/nlp/sudachi/Dictionary.java
@@ -1,5 +1,22 @@
 package com.worksap.nlp.sudachi;
 
+/**
+ * A lexicon and a grammar for morphological analysis.
+ *
+ * This class requires a lot of memory.
+ * When using multiple analyzers, it is recommended to generate only one
+ * instance of this class, and generate multiple tokenizers.
+ *
+ * @see DictionaryFactory
+ * @see Tokenizer
+ * @see AutoCloseable
+ */
 public interface Dictionary extends AutoCloseable {
+
+    /**
+     * Creates a tokenizer instance.
+     *
+     * @return a tokenizer
+     */
     public Tokenizer create();
 }
