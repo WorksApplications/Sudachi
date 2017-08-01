2daff33a5870a602cd1f283c0f9da5b0da528071
takaoka_k <takaoka_k@worksap.co.jp>
2017-08-01 21:55:07 +0900

    Allow to rewrite the connection table

--- src/main/java/com/worksap/nlp/sudachi/dictionary/Grammar.java
+++ src/main/java/com/worksap/nlp/sudachi/dictionary/Grammar.java
@@ -4,7 +4,8 @@ public interface Grammar {
     public int getPartOfSpeechSize();
     public String[] getPartOfSpeechString(short posId);
     public short getPartOfSpeechId(String... pos);
-    public short getConnectCost(int leftId, int rightId);
+    public short getConnectCost(short leftId, short rightId);
+    public void setConnectCost(short leftId, short rightId, short cost);
     public short[] getBOSParameter();
     public short[] getEOSParameter();
     public CharacterCategory getCharacterCategory();
