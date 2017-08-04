95238842191c7ab13d75387014a5af25c7971214
kazuma-t <takaoka_k@worksap.co.jp>
2017-08-04 13:53:23 +0900

    Add javadoc

--- src/main/java/com/worksap/nlp/sudachi/InhibitConnectionPlugin.java
+++ src/main/java/com/worksap/nlp/sudachi/InhibitConnectionPlugin.java
@@ -4,6 +4,24 @@ import java.util.List;
 
 import com.worksap.nlp.sudachi.dictionary.Grammar;
 
+/**
+ * A Plugin for inhibiting the connections.
+ *
+ * <p>{@link Dictionary} initialize this plugin with {@link Settings}.
+ * It can be refered as {@link Plugin#settings}.
+ *
+ * <p>The following is an example of settings.
+ * <pre>{@code
+ *   {
+ *     "class" : "com.worksap.nlp.sudachi.InhibitConnectionPlugin",
+ *     "inhibitedPair" : [ [ 0, 233 ], [435, 332] ]
+ *   }
+ * }</pre>
+ *
+ * {@code inhibitPair} is a list of lists of two numbers.
+ * At each pair, the first number is left-ID and the second is
+ * right-ID of a connection.
+ */
 class InhibitConnectionPlugin extends EditConnectionCostPlugin {
 
     List<List<Integer>> inhibitedPairs;
--- src/main/java/com/worksap/nlp/sudachi/Lattice.java
+++ src/main/java/com/worksap/nlp/sudachi/Lattice.java
@@ -2,11 +2,78 @@ package com.worksap.nlp.sudachi;
 
 import java.util.List;
 
+/**
+ * A graph structure used in the morphological analysis.
+ *
+ * <p>A node of the graph is a morpheme and has a position of the input text.
+ * Only the adjacent nodes are connected.
+ */
 public interface Lattice {
+
+    /**
+     * Returns the nodes which starts at the specified index.
+     *
+     * @param begin the index to the first position in the input text
+     * @return the list of nodes which start at {@code begin}
+     *         or an empty list if there is no node
+     */
     public List<? extends LatticeNode> getNodesWithBegin(int begin);
+
+    /**
+     * Returns the nodes which ends at the specified index.
+     *
+     * <p>The last position of the nodes is {@code end - 1}.
+     *
+     * @param end the index to after the last position in the input text
+     * @return the list of nodes which end at {@code end}
+     *         or an empty list if there is no node
+     */
     public List<? extends LatticeNode> getNodesWithEnd(int end);
+
+    /**
+     * Returns the nodes at the specified index.
+     *
+     * <p>The range of nodes begins at the specified {@code begin} and
+     * extends to the {@code end - 1}.
+     *
+     * @param begin the index to the first position in the input text
+     * @param end the index to after the last position in the input text
+     * @return the list of nodes which start at {@code begin}
+     *         and end at {@code end} or an empty list if there is no node
+     */
     public List<? extends LatticeNode> getNodes(int begin, int end);
+
+    /**
+     * Insert the node at the specified index.
+     *
+     * <p>The range of the node begins at the specified {@code begin} and
+     * extends to the {@code end - 1}.
+     *
+     * @param begin the index to the first position in the input text
+     * @param end the index to after the last position in the input text
+     * @param node the node to be inserted
+     */
     public void insert(int begin, int end, LatticeNode node);
+
+    /**
+     * Remove the node at the specified index.
+     *
+     * <p>The range of the node begins at the specified {@code begin} and
+     * extends to the {@code end - 1}.
+     *
+     * <p>If the node does not exist at the specified position,
+     * this method do nothing.
+     *
+     * @param begin the index to the first position in the input text
+     * @param end the index to after the last position in the input text
+     * @param node the node to be removed
+     */
     public void remove(int begin, int end, LatticeNode node);
+
+    /**
+     * Allocate a new node.
+     *
+     * @return a new node has no information
+     */
     public LatticeNode createNode();
 }
