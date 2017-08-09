d80eb2369f9eeb1b33019e558e10368894a7f0e4
takaoka_k <takaoka_k@worksap.co.jp>
2017-08-09 14:25:26 +0900

    Simplify the codes

--- src/main/java/com/worksap/nlp/sudachi/UTF8InputTextBuilder.java
+++ src/main/java/com/worksap/nlp/sudachi/UTF8InputTextBuilder.java
@@ -11,37 +11,24 @@ import com.worksap.nlp.sudachi.dictionary.Grammar;
 public class UTF8InputTextBuilder implements InputTextBuilder<byte[]> {
 
     private final String originalText;
-    private String modifiedText;
-    private List<Byte> utf8TextBytes;
+    private StringBuilder modifiedText;
     private List<Integer> textOffsets;
-    private List<Integer> byteIndexes;
     
-    private byte[] byteCache;
-    private Grammar grammar;
+    private final Grammar grammar;
     
     public UTF8InputTextBuilder(String text, Grammar grammar) {
         this.grammar = grammar;
         
         originalText = text;
-        modifiedText = text;
-        byteCache = text.getBytes(StandardCharsets.UTF_8);
-        utf8TextBytes = new ArrayList<>(byteCache.length);
-        for (byte b : byteCache) {
-            utf8TextBytes.add(b);
-        }
-        textOffsets = new ArrayList<>(byteCache.length + 1);
-        byteIndexes = new ArrayList<>(byteCache.length + 1);
-        for (int i = 0; i < originalText.length(); i++) {
-            if (Character.isLowSurrogate(originalText.charAt(i))) {
-                continue;
+        modifiedText = new StringBuilder(text);
+        textOffsets = new ArrayList<>(modifiedText.length() + 1);
+        for (int i = 0, j = 0; i < originalText.length(); i++) {
+            if (!Character.isLowSurrogate(originalText.charAt(i))) {
+                j = i;
             }
-            for (int j = 0; j < utf8ByteLength(originalText.codePointAt(i)); j++) {
-                textOffsets.add(i);
-                byteIndexes.add(i);
-            }
+            textOffsets.add(j);
         }
         textOffsets.add(originalText.length());
-        byteIndexes.add(originalText.length());
     }
     
     @Override
@@ -49,59 +36,31 @@ public class UTF8InputTextBuilder implements InputTextBuilder<byte[]> {
         throws StringIndexOutOfBoundsException {
         if (begin < 0)
             throw new StringIndexOutOfBoundsException(begin);
-        if (begin > getText().length())
+        if (begin > modifiedText.length())
             throw new StringIndexOutOfBoundsException("begin > length()");
         if (begin > end)
             throw new StringIndexOutOfBoundsException("begin > end");
         if (begin == end)
             throw new IllegalArgumentException("begin == end");
         
-        if (end > getText().length()) {
-            end = getText().length();
+        if (end > modifiedText.length()) {
+            end = modifiedText.length();
         }
         
-        int offsetBegin = byteIndexes.indexOf(begin);
-        int offsetEnd = byteIndexes.indexOf(end);
-        if (offsetBegin < 0 || offsetEnd < 0) {
-            // character has been removed
-            return;
-        }
-        replaceTextBytesAndOffsets(offsetBegin, offsetEnd, str);
-        byteCache = null;
-        replaceByteIndexes();
-    }
-    
-    private void replaceTextBytesAndOffsets(int begin, int end, String str) {
-        byte[] bytes = str.getBytes(StandardCharsets.UTF_8);
-        int length = bytes.length;
-        int textOffset = textOffsets.get(begin);
+        modifiedText.replace(begin, end, str);
+
+        int offset = textOffsets.get(begin);
+        int length = str.length();
         if (end - begin > length) {
-            utf8TextBytes.subList(begin + length, end).clear();
             textOffsets.subList(begin + length, end).clear();
         }
         for (int i = 0; i < length; i++) {
             if (begin + i < end) {
-                utf8TextBytes.set(begin + i, bytes[i]);
-                textOffsets.set(begin + i, textOffset);
-            }
-            else {
-                utf8TextBytes.add(begin + i, bytes[i]);
-                textOffsets.add(begin + i, textOffset);
+                textOffsets.set(begin + i, offset);
+            } else {
+                textOffsets.add(begin + i, offset);
             }
         }
-    }
-    
-    private void replaceByteIndexes() {
-        byteIndexes.clear();
-        for (int i = 0; i < getText().length(); i++) {
-            if (Character.isLowSurrogate(getText().charAt(i))) {
-                continue;
-            }
-            for (int j = 0; j < utf8ByteLength(getText().codePointAt(i)); j++) {
-                byteIndexes.add(i);
-            }
-        }
-        byteIndexes.add(getText().length());
     }
     
     @Override
@@ -111,50 +70,66 @@ public class UTF8InputTextBuilder implements InputTextBuilder<byte[]> {
     
     @Override
     public String getText() {
-        if (byteCache == null) {
-            modifiedText = new String(getByteText(), StandardCharsets.UTF_8);
-        }
-        return modifiedText;
+        return modifiedText.toString();
     }
     
     @Override
     public UTF8InputText build() {
-        List<List<String>> charCategories = getCharCategoryNames();
-        List<Integer> charCategoryContinuities = getCharCategoryContinuities(charCategories);
-        return new UTF8InputText(grammar, originalText, modifiedText, getByteText(),
-            Collections.unmodifiableList(textOffsets),
-            Collections.unmodifiableList(byteIndexes),
+        String modifiedStringText = getText();
+        byte[] byteText = modifiedStringText.getBytes(StandardCharsets.UTF_8);
+
+        int length = byteText.length;
+        int[] byteIndexes = new int[length + 1];
+        int[] offsets = new int[length + 1];
+        for (int i = 0, j = 0; i < modifiedText.length(); i++) {
+            if (Character.isLowSurrogate(modifiedText.charAt(i))) {
+                continue;
+            }
+            for (int k = 0; k < utf8ByteLength(modifiedText.codePointAt(i)); k++) {
+                byteIndexes[j] = i;
+                offsets[j] = textOffsets.get(i);
+                j++;
+            }
+        }
+        byteIndexes[length] = modifiedStringText.length();
+        offsets[length] = textOffsets.get(textOffsets.size() - 1);
+
+        List<List<String>> charCategories = getCharCategoryNames(modifiedStringText);
+        List<Integer> charCategoryContinuities = getCharCategoryContinuities(modifiedStringText, byteText, byteIndexes, charCategories);
+        return new UTF8InputText(grammar, originalText, modifiedStringText, byteText,
+                                 offsets,
+                                 byteIndexes,
             Collections.unmodifiableList(charCategories),
             Collections.unmodifiableList(charCategoryContinuities));
     }
-    
-    private List<List<String>> getCharCategoryNames() {
-        List<List<String>> charCategoryNames = new ArrayList<>(getText().length());
-        for (int i = 0; i < getText().length(); i++) {
+
+    private List<List<String>> getCharCategoryNames(String text) {
+        List<List<String>> charCategoryNames = new ArrayList<>(text.length());
+        for (int i = 0; i < text.length(); i++) {
             charCategoryNames.add(Collections.unmodifiableList(
-                grammar.getCharacterCategory().getCategoryNames(getText().codePointAt(i))));
+                grammar.getCharacterCategory().getCategoryNames(text.codePointAt(i))));
         }
         return charCategoryNames;
     }
     
-    private List<Integer> getCharCategoryContinuities(List<List<String>> charCategories) {
+    private List<Integer> getCharCategoryContinuities(String text, byte[] byteText, int[] byteIndexes, List<List<String>> charCategories) {
         if (charCategories == null || charCategories.isEmpty()) {
             return Collections.emptyList();
         }
-        List<Integer> charCategoryContinuities = new ArrayList<>(getText().length());
-        for (int offset = 0; offset < getByteText().length; offset++) {
-            charCategoryContinuities.add(getCharCategoryContinuousLength(charCategories, offset));
+        List<Integer> charCategoryContinuities = new ArrayList<>(text.length());
+        for (int offset = 0; offset < byteText.length; offset++) {
+            charCategoryContinuities.add(getCharCategoryContinuousLength(byteText, byteIndexes, charCategories, offset));
         }
         return charCategoryContinuities;
     }
     
-    private int getCharCategoryContinuousLength(List<List<String>> charCategories, int offset) {
+    private int getCharCategoryContinuousLength(byte[] byteText, int[] byteIndexes, List<List<String>> charCategories, int offset) {
         int length;
-        for (length = 0; length < byteCache.length - offset; length++) {
+        for (length = 0; length < byteText.length - offset; length++) {
             boolean found = false;
             // compare each category at offset w/ each category at target (offset + length)
-            List<String> offsetCharCategory = charCategories.get(byteIndexes.get(offset));
-            List<String> targetCharCategory = charCategories.get(byteIndexes.get(offset + length));
+            List<String> offsetCharCategory = charCategories.get(byteIndexes[offset]);
+            List<String> targetCharCategory = charCategories.get(byteIndexes[offset + length]);
             for (int i = 0; i < offsetCharCategory.size(); i++) {
                 for (int j = 0; j < targetCharCategory.size(); j++) {
                     if (offsetCharCategory.get(i).equals(targetCharCategory.get(j))) {
@@ -167,16 +142,6 @@ public class UTF8InputTextBuilder implements InputTextBuilder<byte[]> {
             }
         }
         return length;
-    }
-    
-    private byte[] getByteText() {
-        if (byteCache == null) {
-            byteCache = new byte[utf8TextBytes.size()];
-            for (int i = 0; i < utf8TextBytes.size(); i++) {
-                byteCache[i] = utf8TextBytes.get(i);
-            }
-        }
-        return byteCache;
     }
     
     private int utf8ByteLength(int cp) {
