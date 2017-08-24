60cc966afd3bc69723cb191c61ef0260ea121bbd
kazuma-t <takaoka_k@worksap.co.jp>
2017-08-24 21:11:05 +0900

    Refactor the duplicate code

--- src/main/java/com/worksap/nlp/sudachi/dictionary/UserDictionaryBuilder.java
+++ src/main/java/com/worksap/nlp/sudachi/dictionary/UserDictionaryBuilder.java
@@ -37,75 +37,8 @@ public class UserDictionaryBuilder extends DictionaryBuilder {
     }
 
     @Override
-    void buildLexicon(FileInputStream lexiconInput) throws IOException {
-        LineNumberReader reader
-            = new LineNumberReader(new InputStreamReader(lexiconInput));
-
-        int wordId = 0;
-        for (; ; wordId++) {
-            String line = reader.readLine();
-            if (line == null) {
-                break;
-            }
-
-            String[] cols = line.split(",");
-            if (cols.length != NUMBER_OF_COLUMNS) {
-                System.err.println("Error: invalid format at line "
-                                   + reader.getLineNumber());
-                continue;
-            }
-            for (int i = 0; i < cols.length; i++) {
-                cols[i] = decode(cols[i]);
-            }
-
-            if (cols[0].length() == 0) {
-                System.err.println("Error: headword is empty at line "
-                                   + reader.getLineNumber());
-                continue;
-            }
-            if (cols[0].length() > MAX_STRING_LENGTH
-                || cols[4].length() > MAX_STRING_LENGTH
-                || cols[11].length() > MAX_STRING_LENGTH
-                || cols[12].length() > MAX_STRING_LENGTH) {
-                System.err.println("Error: string is too long at line" + reader.getLineNumber());
-            }
-
-            if (!cols[1].equals("-1")) {
-                // headword
-                byte[] key = cols[0].getBytes(StandardCharsets.UTF_8);
-                if (!trieKeys.containsKey(key)) {
-                    trieKeys.put(key, new ArrayList<Integer>());
-                }
-                trieKeys.get(key).add(wordId);
-                // left-id, right-id, cost
-            }
-            params.add(new Short[] { Short.parseShort(cols[1]),
-                                     Short.parseShort(cols[2]),
-                                     Short.parseShort(cols[3]) });
-
-            List<String> pos = Arrays.asList(cols[5], cols[6], cols[7],
-                                             cols[8], cols[9], cols[10]);
-            short posId = grammar.getPartOfSpeechId(pos);
-            if (posId < 0) {
-                System.err.println("Error: Part of speech is wrong at line  "
-                                   + reader.getLineNumber());
-            }
-
-            WordInfo info
-                = new WordInfo(cols[4], // headword
-                               (short)cols[0].getBytes(StandardCharsets.UTF_8).length,
-                               posId,
-                               cols[12], // normalizedForm
-                               (cols[13].equals("*") ? -1 :Integer.parseInt(cols[13])), // dictionaryFormWordId
-                               "", // dummy
-                               cols[11], // readingForm
-                               parseSplitInfo(cols[15]), // aUnitSplit
-                               parseSplitInfo(cols[16]), // bUnitSplit
-                               parseSplitInfo(cols[17]) // wordStructure
-                               );
-            wordInfos.add(info);
-        }
-        wordSize = wordId;
+    short getPosId(String... posStrings) {
+        return grammar.getPartOfSpeechId(Arrays.asList(posStrings));
     }
 
     /**
