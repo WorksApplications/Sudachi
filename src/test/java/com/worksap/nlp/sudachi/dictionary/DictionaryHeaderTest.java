b1fcaa66ce4671da110cc408adb786d71404d01b
kazuma-t <takaoka_k@worksap.co.jp>
2017-08-28 21:26:05 +0900

    Add the header of dictionaries

---
+++ src/test/java/com/worksap/nlp/sudachi/dictionary/DictionaryHeaderTest.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2017 Works Applications Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.worksap.nlp.sudachi.dictionary;
+
+import java.nio.ByteBuffer;
+import java.io.IOException;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class DictionaryHeaderTest {
+
+    DictionaryHeader header;
+
+    @Before
+    public void setUp() throws IOException {
+        ByteBuffer bytes = DictionaryReader.read("/system.dic");
+        header = new DictionaryHeader(bytes, 0);
+    }
+
+    @Test
+    public void getVersion() {
+        assertEquals(DictionaryVersion.SYSTEM_DICT_VERSION, header.getVersion());
+    }
+
+    @Test
+    public void getCreateTime() {
+        assertTrue(header.getCreateTime() > 0);
+    }
+
+    @Test
+    public void getDescription() {
+        assertEquals("the system dictionary for the unit tests",
+                     header.getDescription());
+    }
+}
