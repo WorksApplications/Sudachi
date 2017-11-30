c9dea2566209d486f4a74f5c2ac0268aca2dc77e
Sorami Hisamoto <hisamoto_s@worksap.co.jp>
2017-11-30 18:52:19 +0900

    Change input text to have more mark sequences

--- src/test/java/com/worksap/nlp/sudachi/ProlongedSoundMarkInputTextPluginTest.java
+++ src/test/java/com/worksap/nlp/sudachi/ProlongedSoundMarkInputTextPluginTest.java
@@ -99,8 +99,8 @@ public class ProlongedSoundMarkInputTextPluginTest {
 
     @Test
     public void combineContinuousProlongedSoundMarksMultipleTimes() {
-        final String ORIGINAL_TEXT = "スーーーパーーーーー";
-        final String NORMALIZED_TEXT = "スーパー";
+        final String ORIGINAL_TEXT = "エーービーーーシーーーー";
+        final String NORMALIZED_TEXT = "エービーシー";
         builder = new UTF8InputTextBuilder(ORIGINAL_TEXT, new MockGrammar());
         plugin.rewrite(builder);
         text = builder.build();
@@ -108,19 +108,23 @@ public class ProlongedSoundMarkInputTextPluginTest {
         assertThat(text.getOriginalText(), is(ORIGINAL_TEXT));
         assertThat(text.getText(), is(NORMALIZED_TEXT));
         byte[] bytes = text.getByteText();
-        assertThat(bytes.length, is(12));
+        assertThat(bytes.length, is(18));
         assertArrayEquals(
                 new byte[] {
-                        (byte)0xE3, (byte)0x82, (byte)0xB9, (byte)0xE3,
+                        (byte)0xE3, (byte)0x82, (byte)0xA8, (byte)0xE3,
                         (byte)0x83, (byte)0xBC, (byte)0xE3, (byte)0x83,
-                        (byte)0x91, (byte)0xE3, (byte)0x83, (byte)0xBC
+                        (byte)0x93, (byte)0xE3, (byte)0x83, (byte)0xBC,
+                        (byte)0xE3, (byte)0x82, (byte)0xB7, (byte)0xE3,
+                        (byte)0x83, (byte)0xBC
                 }, bytes
         );
         assertThat(text.getOriginalIndex(0), is(0));
         assertThat(text.getOriginalIndex(3), is(1));
-        assertThat(text.getOriginalIndex(6), is(4));
-        assertThat(text.getOriginalIndex(9), is(5));
-        assertThat(text.getOriginalIndex(12), is(10));
+        assertThat(text.getOriginalIndex(6), is(3));
+        assertThat(text.getOriginalIndex(9), is(4));
+        assertThat(text.getOriginalIndex(12), is(7));
+        assertThat(text.getOriginalIndex(15), is(8));
+        assertThat(text.getOriginalIndex(18), is(12));
     }
 
     class MockGrammar implements Grammar {
