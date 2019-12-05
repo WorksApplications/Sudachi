aaa0541cbd403ea34f8ebb07943f3a56938b6616
Libo Zeng <ringil.sword@gmail.com>
2019-12-05 08:30:24 -0500

    use System.lineSeparator instead

--- src/test/java/com/worksap/nlp/sudachi/dictionary/DictionaryHeaderPrinterTest.java
+++ src/test/java/com/worksap/nlp/sudachi/dictionary/DictionaryHeaderPrinterTest.java
@@ -48,7 +48,7 @@ public class DictionaryHeaderPrinterTest {
         String[] actuals;
         try (ByteArrayOutputStream output = new ByteArrayOutputStream(); PrintStream ps = new PrintStream(output)) {
             DictionaryHeaderPrinter.printHeader(inputFile.getPath(), ps);
-            actuals = output.toString().split("[\r\n]+");
+            actuals = output.toString().split(System.lineSeparator());
         }
         assertThat(actuals.length, is(4));
         assertThat(actuals[0], is("filename: " + inputFile.getPath()));
@@ -63,7 +63,7 @@ public class DictionaryHeaderPrinterTest {
         String[] actuals;
         try (ByteArrayOutputStream output = new ByteArrayOutputStream(); PrintStream ps = new PrintStream(output)) {
             DictionaryHeaderPrinter.printHeader(inputFile.getPath(), ps);
-            actuals = output.toString().split("[\r\n]+");
+            actuals = output.toString().split(System.lineSeparator());
         }
         assertThat(actuals.length, is(4));
         assertThat(actuals[0], is("filename: " + inputFile.getPath()));
@@ -78,7 +78,7 @@ public class DictionaryHeaderPrinterTest {
         String[] actuals;
         try (ByteArrayOutputStream output = new ByteArrayOutputStream(); PrintStream ps = new PrintStream(output)) {
             DictionaryHeaderPrinter.printHeader(inputFile.getPath(), ps);
-            actuals = output.toString().split("[\r\n]+");
+            actuals = output.toString().split(System.lineSeparator());
         }
         assertThat(actuals.length, is(2));
         assertThat(actuals[0], is("filename: " + inputFile.getPath().replaceAll("\r", "")));
