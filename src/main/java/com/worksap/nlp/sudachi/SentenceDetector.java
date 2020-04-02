56ea6a9ac42462042b8d04e3140c97abc807885c
Kazuma TAKAOKA <takaoka_k@worksap.co.jp>
2020-04-02 19:40:54 +0900

    Add a sentence break detector

---
+++ src/main/java/com/worksap/nlp/sudachi/SentenceDetector.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2020 Works Applications Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.worksap.nlp.sudachi;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SentenceDetector {
+
+    static private String PERIODS = "。|？|！|♪|…|\\?|\\!";
+    static private String DOT = "(\\.|．)";
+    static private String CDOT = "・";
+    static private String COMMA = "(,|，|、)";
+    static private String BR_TAG = "(<br>|<BR>){2,}";
+    static private String ALPHABET_OR_NUMBER = "[a-z]|[A-Z]|[0-9]|[ａ-ｚ]|[Ａ-Ｚ]|[０-９]|〇|一|二|三|四|五|六|七|八|九|十|百|千|万|億|兆";
+    static private Pattern SENTENCE_BREAKER = Pattern
+            .compile("(" + PERIODS + "|" + CDOT + "{3,}+|((?<!(" + ALPHABET_OR_NUMBER + "))" + DOT + "(?!("
+                    + ALPHABET_OR_NUMBER + "|" + COMMA + "))))(" + DOT + "|" + PERIODS + ")*|" + BR_TAG);
+
+    static private String OPEN_PARENTHESIS = "\\(|\\{|｛|\\[|（|「|【|『|［|≪|〔|“";
+    static private String CLOSE_PARENTHESIS = "\\)|\\}|\\]|）|」|｝|】|』|］|〕|≫|”";
+    static private Pattern PARENTHESIS = Pattern.compile("(" + OPEN_PARENTHESIS + ")|(" + CLOSE_PARENTHESIS + ")");
+
+    static private String ITEMIZE_HEADER = "(" + ALPHABET_OR_NUMBER + ")" + "(" + DOT + ")";
+
+    static final int DEFAULT_LIMIT = 4096;
+
+    private int limit;
+
+    public SentenceDetector() {
+        this(-1);
+    }
+
+    public SentenceDetector(int limit) {
+        this.limit = (limit > 0) ? limit : DEFAULT_LIMIT;
+    }
+
+    public int getEOS(String input) {
+        if (input.isEmpty()) {
+            return 1;
+        }
+
+        String s = (input.length() > limit) ? input.substring(0, limit) : input;
+        Matcher matcher = SENTENCE_BREAKER.matcher(s);
+        while (matcher.find()) {
+            int eos = matcher.end();
+            if (parenthesisLevel(s.substring(0, eos)) == 0) {
+                if (eos < s.length()) {
+                    eos += prohibitedBOS(s.substring(eos));
+                }
+                if (s.substring(0, eos).matches(ITEMIZE_HEADER)) {
+                    continue;
+                }
+                if (eos < s.length() && isContinuousPhrase(s, eos)) {
+                    continue;
+                }
+                return eos;
+            }
+        }
+
+        final Pattern SPACES = Pattern.compile(".+\\s+");
+        Matcher m = SPACES.matcher(s);
+        if (m.find()) {
+            return m.end();
+        }
+
+        return Math.min(input.length(), limit);
+    }
+
+    int parenthesisLevel(String s) {
+        Matcher matcher = PARENTHESIS.matcher(s);
+        int level = 0;
+        while (matcher.find()) {
+            if (matcher.group(1) != null) { // open
+                level++;
+            } else {
+                level--;
+            }
+            if (level < 0) {
+                level = 0;
+            }
+        }
+        return level;
+    }
+
+    int prohibitedBOS(String s) {
+        final Pattern PROHIBITED_BOS = Pattern.compile("\\A(" + CLOSE_PARENTHESIS + "|" + COMMA + "|" + PERIODS + ")+");
+        Matcher m = PROHIBITED_BOS.matcher(s);
+        return (m.find()) ? m.end() : 0;
+    }
+
+    boolean isContinuousPhrase(String s, int eos) {
+        final Pattern QUOTE_MARKER = Pattern.compile("(！|？|\\!|\\?|" + CLOSE_PARENTHESIS + ")(と|っ|です)");
+        Matcher m = QUOTE_MARKER.matcher(s);
+        if (m.find(eos - 1) && m.start() == eos - 1) {
+            return true;
+        }
+
+        final Pattern EOS_ITEMIZE_HEADER = Pattern.compile(ITEMIZE_HEADER + "\\z");
+        char c = s.charAt(eos);
+        if ((c == 'と' || c == 'や' || c == 'の') && EOS_ITEMIZE_HEADER.matcher(s.substring(0, eos)).find()) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/com/worksap/nlp/sudachi/SentenceDetectorTest.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2020 Works Applications Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.worksap.nlp.sudachi;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SentenceDetectorTest {
+
+    private SentenceDetector detector;
+
+    @Before
+    public void setUp() {
+        detector = new SentenceDetector();
+    }
+
+    @Test
+    public void getEOS() {
+        assertThat(detector.getEOS("あいう。えお。"), is(4));
+        assertThat(detector.getEOS("あいうえお"), is(5));
+        assertThat(detector.getEOS(""), is(1));
+    }
+
+    @Test
+    public void getEOSWithLimit() {
+        detector = new SentenceDetector(5);
+        assertThat(detector.getEOS("あい。うえお。"), is(3));
+        assertThat(detector.getEOS("あいうえおか"), is(5));
+        assertThat(detector.getEOS("あいうえお。"), is(5));
+        assertThat(detector.getEOS("あい うえお"), is(3));
+        assertThat(detector.getEOS("あ い うえお"), is(4));
+    }
+
+    @Test
+    public void getEOSWithPeriod() {
+        assertThat(detector.getEOS("あいう.えお"), is(4));
+        assertThat(detector.getEOS("3.141"), is(5));
+        assertThat(detector.getEOS("四百十．〇"), is(5));
+    }
+
+    @Test
+    public void getEOSWithParenthesis() {
+        assertThat(detector.getEOS("あ（いう。え）お"), is(8));
+        assertThat(detector.getEOS("（あ（いう）。え）お"), is(10));
+        assertThat(detector.getEOS("あ（いう）。えお"), is(6));
+    }
+
+    @Test
+    public void getEOSWithProhibitedBOS() {
+        assertThat(detector.getEOS("あいう?えお"), is(4));
+        assertThat(detector.getEOS("あいう?)えお"), is(5));
+        assertThat(detector.getEOS("あいう?,えお"), is(5));
+    }
+
+    @Test
+    public void getEOSWithContinuousPhrase() {
+        assertThat(detector.getEOS("あいう?です。"), is(7));
+        assertThat(detector.getEOS("あいう?って。"), is(7));
+        assertThat(detector.getEOS("あいう?という。"), is(8));
+        assertThat(detector.getEOS("あいう?の?です。"), is(4));
+
+        assertThat(detector.getEOS("1.と2.が。"), is(7));
+        assertThat(detector.getEOS("1.やb.が。"), is(7));
+        assertThat(detector.getEOS("1.の12.が。"), is(8));
+    }
+
+}
