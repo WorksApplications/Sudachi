10247b09a1c023ff5299d84c21bf19cc2205c5e5
Arseny Tolmachev <arseny@kotonoha.ws>
2021-12-14 17:55:00 +0900

    add test for all fields

--- src/test/java/com/worksap/nlp/sudachi/dictionary/build/SystemDicTest.kt
+++ src/test/java/com/worksap/nlp/sudachi/dictionary/build/SystemDicTest.kt
@@ -1,6 +1,7 @@
 package com.worksap.nlp.sudachi.dictionary.build
 
 import com.worksap.nlp.sudachi.dictionary.BinaryDictionary
+import com.worksap.nlp.sudachi.dictionary.POS
 import kotlin.test.Test
 import kotlin.test.assertContentEquals
 import kotlin.test.assertEquals
@@ -17,6 +18,30 @@ class SystemDicTest {
         val dic = BinaryDictionary(data.buffer())
         assertEquals(1, dic.grammar.partOfSpeechSize)
         assertEquals(1, dic.lexicon.size())
+    }
+
+    @Test
+    fun fields() {
+        val bldr = DicBuilder.system().matrix(javaClass.getResource("test.matrix"))
+        val data = BytesChannel()
+        repeat(10) { bldr.lexicon(javaClass.getResource("one.csv")) }
+        bldr.lexicon("南,1,1,4675,南,名詞,普通名詞,一般,*,*,*,ミナミ,西,5,C,0/1,2/3,4/5,6/7".byteInputStream())
+            .build(data)
+        val dic = BinaryDictionary(data.buffer())
+        assertEquals(11, dic.lexicon.size())
+        assertEquals(POS("名詞", "普通名詞", "一般", "*", "*", "*"), dic.grammar.getPartOfSpeechString(0))
+        val wi = dic.lexicon.getWordInfo(10)
+        assertEquals(wi.surface, "南")
+        assertEquals(wi.length, 3)
+        assertEquals(wi.posId, 0)
+        assertEquals(wi.dictionaryFormWordId, 5)
+        assertEquals(wi.dictionaryForm, "東")
+        assertEquals(wi.normalizedForm, "西")
+        assertEquals(wi.readingForm, "ミナミ")
+        assertContentEquals(wi.aunitSplit, intArrayOf(0, 1))
+        assertContentEquals(wi.bunitSplit, intArrayOf(2, 3))
+        assertContentEquals(wi.wordStructure, intArrayOf(4, 5))
+        assertContentEquals(wi.synonymGoupIds, intArrayOf(6, 7))
     }
 
     @Test
