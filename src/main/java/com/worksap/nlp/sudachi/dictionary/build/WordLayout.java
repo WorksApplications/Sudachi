c3d090cd00377bafe62725e6f8f12ad7bd904171
Arseny Tolmachev <arseny@kotonoha.ws>
2022-04-08 16:54:42 +0900

    add documentation for WordLayout

--- src/main/java/com/worksap/nlp/sudachi/dictionary/build/WordLayout.java
+++ src/main/java/com/worksap/nlp/sudachi/dictionary/build/WordLayout.java
@@ -6,7 +6,25 @@ import java.io.IOException;
 import java.nio.channels.WritableByteChannel;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.StringJoiner;
 
+/**
+ * <p>
+ * Lays out dictionary words so that they will form correct {@link StringPtr} instances.
+ * That means taking into account the required alignment for strings with larger sizes.
+ * Aligning strings produces wasted space in form of padding, which is kept track with free list approach.
+ *
+ * <p>
+ * The main API is {@link #add(String)} method which should be called for all strings.
+ * The method should be called for strings sorted in descending order by length, otherwise padding between aligned strings
+ * would not be utilized correctly.
+ * The returned {@link StringPtr}s will be correct in any case.
+ *
+ * <p>
+ * The current implementation is relatively fast, but can be made even faster by using sorted multiset collection.
+ * JVM standard library does not have one, so the current implementation sorts free list while keeping track if the sort
+ * is needed and guarding against relatively expensive checking free lists with additional conditions.
+ */
 public class WordLayout {
     private final UnicodeBufferResizeable buffer = new UnicodeBufferResizeable();
     private final ArrayList<FreeSpace> free = new ArrayList<>();
@@ -26,7 +44,20 @@ public class WordLayout {
         return StringPtr.checked(length, offset);
     }
 
-    private int allocateAligned(int length, int alignment, int start, int end) {
+    /**
+     * Allocates a segment of utf-16 code units in a specified block, taking in account requested alignment.
+     *
+     * Alignment can skip some space in the beginning of the block because of padding.
+     * That space will be placed into free lists.
+     * Nothing will be placed in the free lists if the allocation is not possible.
+     *
+     * @param length requested length of segment
+     * @param alignment requested alignment of segment
+     * @param start start of the block of memory to use
+     * @param end end of the block of memory to use
+     * @return offset of the aligned data or -1 if allocation is impossible
+     */
+    private int allocateInBlock(int length, int alignment, int start, int end) {
         int requiredAlignment = Math.max(0, alignment - 1);
         int alignmentStep = 1 << requiredAlignment;
         int alignmentMask = alignmentStep - 1;
@@ -44,11 +75,25 @@ public class WordLayout {
             assert padding > 0;
             free.add(new FreeSpace(start, padding));
             freeDirty = true;
-            maxLength = Math.max(maxLength, padding - (start & alignmentMask));
+            int estimated = availableMaxLength(padding);
+            maxLength = Math.max(maxLength, estimated);
         }
         return alignedStart;
     }
 
+    /**
+     * Allocates a slot of {@code length} bytes, alignment with {@code alignment}.
+     * It first considers free slots created by previous allocations, if none is valid.
+     *
+     * Current implementation is prone to creating "holes" of 1-length, which are almost impossible to fill
+     * from the usual dictionaries.
+     * Most emoji take 2 code units and words which are not substrings of another word are usually longer.
+     * The current implementation wastes ~32k holes in ~42M dictionary, which is ~0.1% of total space.
+     *
+     * @param length number of byte
+     * @param alignment requested alignment
+     * @return offset in utf-16 code units to the location of the requested block
+     */
     private int allocate(int length, int alignment) {
         if (length <= maxLength) {
             if (freeDirty) {
@@ -67,14 +112,17 @@ public class WordLayout {
                     continue;
                 }
                 int end = fs.start + fs.length;
-                int start = allocateAligned(length, alignment, fs.start, end);
+                int start = allocateInBlock(length, alignment, fs.start, end);
                 if (start != -1) {
                     int remaining = end - start - length;
                     if (remaining > 0) {
                         fs.start = start + length;
                         fs.length = remaining;
                         freeDirty = true;
-                        maxLength = computeNewMaxLength(i);
+                        // we need to recompute maxLength only if modifying the last (maximum) element in free lists
+                        if (i == numFree - 1) {
+                            maxLength = computeNewMaxLength(i);
+                        }
                     } else {
                         free.remove(i);
                         maxLength = computeNewMaxLength(numFree - 2);
@@ -86,21 +134,34 @@ public class WordLayout {
         }
 
 
-        int alignedStart = allocateAligned(length, alignment, pointer, Integer.MAX_VALUE);
+        int alignedStart = allocateInBlock(length, alignment, pointer, Integer.MAX_VALUE);
         assert alignedStart != -1;
         pointer = alignedStart + length;
         return alignedStart;
     }
 
-    private int estimatedLength(int length) {
-        if (length <= 20) {
+    /**
+     * Returns available max length for a hole
+     * @param length hole length
+     * @return length of an element which can be allocated using any alignment
+     */
+    private static int availableMaxLength(int length) {
+        int simple = StringPtr.MAX_SIMPLE_LENGTH + 1;
+        if (length <= simple) {
             return length;
         }
-        int clz = Integer.numberOfLeadingZeros(length);
-        return 1 << (31 - clz);
+        int clz = Integer.numberOfLeadingZeros(length - simple);
+        int candidateLength = 1 << (31 - clz);
+        return Math.max(simple, candidateLength + simple);
     }
 
-    private int computeNewMaxLength(int i) {
+    /**
+     * Compute new maximum length which can be handled by free lists.
+     * Should be called if the last element of free lists was updated.
+     * @param index index in free lists of the element which needs to be considered
+     * @return new maximum length that can be handled by free lists
+     */
+    private int computeNewMaxLength(int index) {
         // assumption: free is sorted ascending my length (except i-th item)
         int freeLength = free.size();
         // if new size of free array is 0, then it's -1
@@ -108,11 +169,11 @@ public class WordLayout {
             return -1;
         }
 
-        int newLength = estimatedLength(free.get(i).length);
+        int newLength = availableMaxLength(free.get(index).length);
         // more than 1 element: maximum of newly computed length and the previous value
         if (freeLength > 1) {
-            FreeSpace prevSpace = free.get(i - 1);
-            int fixedLength = estimatedLength(prevSpace.length);
+            FreeSpace prevSpace = free.get(index - 1);
+            int fixedLength = availableMaxLength(prevSpace.length);
             return Math.max(fixedLength, newLength);
         } else {
             // otherwise, simply new value
@@ -135,9 +196,17 @@ public class WordLayout {
 
         @Override
         public int compareTo(FreeSpace o) {
-            int cval = Integer.compare(length, o.length);
-            if (cval != 0) return cval;
+            int comparison = Integer.compare(length, o.length);
+            if (comparison != 0) return comparison;
             return Integer.compare(start, o.start);
+        }
+
+        @Override
+        public String toString() {
+            return new StringJoiner(", ", FreeSpace.class.getSimpleName() + "[", "]")
+                    .add("start=" + start)
+                    .add("length=" + length)
+                    .toString();
         }
     }
 
