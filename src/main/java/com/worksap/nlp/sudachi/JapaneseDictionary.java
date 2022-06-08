3626aee512d1b45e4b6f312abf9f2e3870184c51
Arseny Tolmachev <arseny@kotonoha.ws>
2022-06-08 16:08:03 +0900

    remove unused imports

--- src/main/java/com/worksap/nlp/sudachi/JapaneseDictionary.java
+++ src/main/java/com/worksap/nlp/sudachi/JapaneseDictionary.java
@@ -23,7 +23,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
