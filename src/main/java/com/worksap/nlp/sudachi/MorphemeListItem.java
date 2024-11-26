08c09cc2f18b96286d31f333d1a517169ec7c193
mh-northlander <mh.northlander+github@gmail.com>
2024-11-26 11:25:00 +0900

    rm unused import

--- src/main/java/com/worksap/nlp/sudachi/MorphemeListItem.java
+++ src/main/java/com/worksap/nlp/sudachi/MorphemeListItem.java
@@ -18,7 +18,6 @@ package com.worksap.nlp.sudachi;
 
 import java.util.List;
 
-import com.worksap.nlp.sudachi.dictionary.POS;
 import com.worksap.nlp.sudachi.dictionary.Grammar;
 import com.worksap.nlp.sudachi.dictionary.WordInfo;
 
